a3135c0acf3ca256e4e2e071af9a88ddeca2c61f
Megha Malik <meghamalik888@gmail.com>
2025-10-26 18:39:16 +0530

    Create index.php for basic PHP web page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html lang='en'>";
+echo "<head><meta charset='UTF-8'><title>My PHP App</title></head>";
+echo "<body>";
+echo "<h1>Welcome to My PHP App!</h1>";
+echo "<p>This page is generated using PHP.</p>";
+echo "</body>";
+echo "</html>";
+?>
